768c56de6273fa8b7cbc0715a8b9ee6163e0ab86
deepak Sharma <deepaksharma4715@gmail.com>
2024-06-25 22:32:28 +0530

    programs practice

--- Basic/Array.cpp
+++ Basic/Array.cpp
@@ -1,152 +1,82 @@
-#include <iostream>
-#include <array>
-#include <vector>
-#include <deque>
-#include <map>
-#include <set>
-
-#include<unordered_set>
-using namespace std;
+#include<iostream>
+#include<string>
+using namespace std ;
 
 
 
+    int  index(char& c){
 
+     int index = 0 ;
 
-int main() {
+         if((c >='a' && c<='z') ){
+            index = c - 'a';
+         }
+         else if ( (c >='A' && c <='Z'))
+         {
+            index = c -'A';
 
+         }
 
-unordered_set<string> uset ; 
-uset.insert("bingo ");
-uset.insert("is ");
-uset.insert("my ");
-uset.insert("namo ");
+         return index;
+    }
+    bool isAlpha(char& c ){
 
+        if((c >='a' && c<='z') || (c >='A' && c <='Z') ){
+            return true;
+        }
 
-for (auto it = uset.begin(); it!= uset.end() ; it++ ){
+        return false ;
+    }
+    
+    bool ValidPD(string& s , int start , int end ){
+        cout <<" valid pd called "<<endl;
 
-    cout<<*it <<"  "<<endl;
-}
+        while(!isAlpha(s[start]) && start <=end ){
+            cout<<" start alpha called " <<endl;
+            start++;
+        }
 
+        while(!isAlpha(s[end]) && start >= end ){
+               cout<<" end alpha called " <<endl;
+            end--;
+        }
+         
+         if(start > end ){
+            return true ;
+         }
 
-/*
-
- multimap<int,int> mset ; 
-
-mset.insert(pair<int,int>(1,3)) ; 
-mset.insert(pair<int,int>(2,4)) ; 
-mset.insert(pair<int,int>(1,2)) ; 
-
-
-
-
-for (auto it = mset.begin(); it!= mset.end() ; it++ ){
-
-    cout<<it->first << " & "<< it->second <<endl;
-}
-
-
-  
-
-
-
-
-
-
-/*
-   map<int, int> gquiz1;
- 
-    // insert elements in random order
-    gquiz1.insert(pair<int, int>(4, 20));
-    gquiz1.insert(pair<int, int>(5, 50));
-    gquiz1.insert(pair<int, int>(6, 50));
-    gquiz1.insert(pair<int, int>(1, 40));
-    gquiz1.insert(pair<int, int>(2, 30));
-    gquiz1.insert(pair<int, int>(3, 60));
-       gquiz1.insert(pair<int, int>(4, 60));
- 
-
-for (auto it = gquiz1.begin() ; it != gquiz1.end() ; it++)
- {
-    cout << it->first <<"  " << it->second <<endl;
- }
-
-/*
-deque<int> testDqueue ; 
-
-testDqueue.push_front(1);
-testDqueue.push_front(2);
-testDqueue.push_back(3);
-testDqueue.push_back(4);
-
-
- for (auto it = testDqueue.begin() ; it != testDqueue.end() ; it++)
- {
-    cout << *it <<"  ";
- }
-
-
-
-
-
-/*
-vector<int> arrayV(5, -2 );
-
-for (int i =0 ; i < 5 ; i++){
-    arrayV.push_back(i);
-}
-for (int i =0 ; i < arrayV.size() ; i++){
-    cout <<arrayV[i] << " " ; 
-}
-
-cout <<" printing from auto itertor -> "<<endl ; 
-for (auto it = arrayV.begin() ; it != arrayV.end() ; it++){
-    cout<< *it <<endl;
-}
-cout <<" max size " << arrayV.max_size() <<endl;
-cout <<"  size " << arrayV.size() <<endl;
-cout <<" capacity " << arrayV.capacity() <<endl;
-
-
-
- //sequence container 
-//vector 
-
-
- // array 
-  /*
-  array<int, 5 > arr ; 
-  arr.fill(-1);
-
-  arr[3] = 5 ;
-
-  for (auto x : arr){
-    cout << x <<" " ; 
-  }
-*/
-
-/*
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-
-    int size = sizeof(a)/sizeof(int) ; 
-
-    cout <<" print the size " << size << endl; 
-
-    for(int i =0 ; i < size/2 ; i++){
-       
-        int temp = a[size -i -1 ];
-        a[size -i -1 ] = a[i] ;
-        a[i] = temp ; 
-
+         if(index(s[start]) == index(s[end])){
+            return ValidPD(s , start+1, end-1);
+         }
+         else{
+            return false ;
+         }
 
     }
 
-    for(int i =0 ; i < size ; i++){
-       
-     cout<<" " << a[i] ; 
+    bool isPalindrome(string s) {
 
+        if(s.length() <=1 ){
+            return true;
+        }
+        cout<<"is pal called "<<endl;
+      //    transform(s.begin(), s.end(), s.begin(), ::tolower); 
+        bool result = ValidPD(s , 0 , s.length()-1);
 
+        return result ;
+        
     }
-*/
 
-    return 0 ; 
-}
+
+    int main(){
+        
+      //  string input = "A man, a plan, a canal: Panama";
+       string input = "caac";
+        cout <<"hellow world " <<endl;
+
+        cout << "result = " << isPalindrome(input);
+
+
+
+return 0 ;
+    }
--- Basic/DP/subsetzSum.cpp
+++ Basic/DP/subsetzSum.cpp
@@ -1,51 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
-    int solve(int arr[], int sum , int size,  vector<vector<int>>& dp){
-        
-        if( sum==0){
-            return 1;
-        }
-        if(size < 0 || sum < 0){
-            return 0;
-        }
-        
-        if(  dp[size][sum] != -1){
-              return dp[size][sum]; 
-        }
-        
-        int include =  solve(arr, sum-arr[size] , size-1,dp);
-        int exclude = solve (arr, sum,size-1,dp);
-        
-        dp[size][sum] = (include || exclude);
-        return   dp[size][sum];
-        
-        
-        
-        
+  
+    vector<string> strtok(string& st , char c){
+
+      string s = "";
+      vector<string> vs;
+      for(int i =0 ; i < st.length(); i++){
+      
+          if(st[i] == c){
+        //    cout << s<<endl;
+            vs.push_back(s);
+            s.clear();
+          }
+          else{
+         
+           s.push_back(st[i]);
+          //    cout << " push back ->" << s <<endl;
+          }
+      }
+      
+      vs.push_back(s);
+      return vs;
+    }
+
+    bool wordPattern(string pattern, string s) {
+
+        unordered_map<char, string> mp;
+        unordered_map<string, char> mp2;
+        bool result = true ;
+       vector<string> vs = strtok(s, ' ');
+
+       if(pattern.length() != vs.size()){
+    //    cout<<" size is not same "<<endl;
+        return false ;
+       }
+ 
+    for(int i =0 ; i < pattern.length();i++){
+
+          if(mp.count(pattern[i])){
+
+              if(mp[pattern[i]] != vs[i]){
+                result =  false ;
+                break;
+              }
+
+          }
+          else{
+            mp[pattern[i]] = vs[i];
+
+             cout<<" vs[i]" <<vs[i] <<" count "<<mp2.count(vs[i]) <<endl;
+           if(mp2.count(vs[i])){
+                result = false ;
+                cout << "result return false "<<endl;
+                break;
+            }
+            mp2[vs[i]] = pattern[i];
+
+          }
+
+    }
+
+
+
+
+        return result;
     }
     int main()
     {
-        // code here
-        int N =4 ;
-        int arr[] = {1,5,11,5};
-
-
-        int sum =0; 
-        for(int i =0 ; i < N ;i++){
-            sum = sum + arr[i];
-        }
-         
-        if(sum%2 ==1){
-            cout<< "sum is odd value"<<sum<<" "<<(sum%2)<<endl;
-            return 0;
-        } 
-        
-        vector<vector<int>>dp(N+1,vector<int>(sum+1,-1));
-        
-        int  result = solve(arr ,sum/2, N-1,dp);
-        cout <<"output is "<< result<<endl;
+       string a ="abba" ;
+       string b = "dog dog dog dog" ; 
+     bool   result = wordPattern(a,b);
+     cout<< "result = "<< result<<endl; 
         return result ;
     }
